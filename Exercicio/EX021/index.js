b24cfd87aff7fdde1b6f6847e47ad0627b402288
Gabriella Muniz <gabriellamunizzz08@gmail.com>
2023-09-09 13:40:29 -0300

    Initial commit

---
+++ Exercicio/EX021/index.js
@@ -0,0 +1,10 @@
+let nomes = ["Gabriella", "Fernanda", "Carol", "Cristiane", "Pablo", "Fernando", "Barbara", "Otávio", "Andressa", "Yasmim"];
+let idades = [25, 30, 22, 28, 29, 35, 27, 24, 31, 26];
+let coresFavoritas = ["vermelho", "amarelo", "rosa", "branco", "laranja", "preto", "azul", "cinza", "turquesa", "marrom"];
+
+idades[7] = 29; 
+coresFavoritas[2] = "preto"; 
+
+console.log("Lista de Nomes:", nomes);
+console.log("Lista de Idades:", idades);
+console.log("Lista de Cores Favoritas:", coresFavoritas);
